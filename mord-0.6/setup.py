f3ea4654106c07e636bf5a276466a1530363f793
Ashish Pant <ashish.pant@in.ey.com>
2023-03-21 09:59:38 +0530

    adv statistics

---
+++ mord-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mord',
+    version="0.6",
+    description='Ordinal regression models',
+    long_description=open('README.rst').read(),
+    author='Fabian Pedregosa',
+    author_email='f@bianp.net',
+    url='https://pypi.python.org/pypi/mord',
+    packages=['mord'],
+    requires=['numpy', 'scipy', "sklearn"],
+)
